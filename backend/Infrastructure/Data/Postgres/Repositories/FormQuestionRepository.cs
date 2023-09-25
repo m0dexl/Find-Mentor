424aee90dcef7f65bbee66acb8ab10991bcbdc5f
m0dexl <yahya.altungul@gmail.com>
2023-09-25 15:43:53 +0300

    mentor-questions-formquestion repositories and interfaces

---
+++ backend/Infrastructure/Data/Postgres/Repositories/FormQuestionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.EntityFramework;
+using Infrastructure.Data.Postgres.Repositories.Base;
+using Infrastructure.Data.Postgres.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Postgres.Repositories
+{
+    public class FormQuestionRepository : Repository<FormQuestion, int>, IFormQuestionRepository
+    {
+        public FormQuestionRepository(PostgresContext postgresContext) : base(postgresContext)
+        {
+        }
+
+        public async Task<IList<FormQuestion>> GetAllAsync(Expression<Func<FormQuestion, bool>>? filter = null)
+        {
+            var user = PostgresContext.Set<FormQuestion>();
+            return filter == null
+                ? await user.ToListAsync()
+                : await user.Where(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IList<FormQuestion>> GetByFormQuestionIdAsync(int id)
+        {
+            // Tüm biletleri çekmek için GetAllAsync'i kullanabiliriz
+            return await GetAllAsync();
+        }
+    }
+}
---
+++ backend/Infrastructure/Data/Postgres/Repositories/QuestionsRepository.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.EntityFramework;
+using Infrastructure.Data.Postgres.Repositories.Base;
+using Infrastructure.Data.Postgres.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Postgres.Repositories
+{
+    public class QuestionsRepository : Repository<Questions, int>, IQuestionsRepository
+    {
+        public QuestionsRepository(PostgresContext postgresContext) : base(postgresContext)
+        {
+        }
+        public async Task<IList<Questions>> GetAllAsync(Expression<Func<Questions, bool>>? filter = null)
+        {
+            var user = PostgresContext.Set<Questions>();
+            return filter == null
+                ? await user.ToListAsync()
+                : await user.Where(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IList<Questions>> GetByQuestionsIdAsync(int id)
+        {
+            // Tüm biletleri çekmek için GetAllAsync'i kullanabiliriz
+            return await GetAllAsync();
+        }
+    }
+}
