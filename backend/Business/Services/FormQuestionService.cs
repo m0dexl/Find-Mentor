f405478fae8fecf490d0430c8fcd53f2f90516f1
m0dexl <yahya.altungul@gmail.com>
2023-09-28 14:56:41 +0300

    adding formquestionservices

---
+++ backend/Business/Services/FormQuestionService.cs
@@ -0,0 +1,26 @@
+﻿using Business.Models.Response;
+using Business.Services.Base;
+using Business.Services.Base.Interface;
+using Business.Services.Interface;
+using Business.Utilities.Mapping.Interface;
+using Infrastructure.Data.Postgres;
+using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.Repositories.Base.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class FormQuestionService : BaseService<FormQuestion, int, FormQuestionInfoDto>, IFormQuestionService
+    {
+        public FormQuestionService(IUnitOfWork unitOfWork, IMapperHelper mapperHelper) : base(unitOfWork, unitOfWork.FormQuestion, mapperHelper)
+        {
+
+        }
+
+
+    }
+}
